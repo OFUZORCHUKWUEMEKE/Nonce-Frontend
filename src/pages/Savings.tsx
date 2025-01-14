e5e07fd20ad93017f36d27a2637528d88d801a7d
OFUZOR CHUKWUEMEKE <uniben2018@gmail.com>
2025-01-14 22:06:14 +0000

    savings

--- src/pages/Savings.tsx
+++ src/pages/Savings.tsx
@@ -10,7 +10,11 @@ const Savings = () => {
         <DashboardLayout>
             <main className="w-full px-10 py-10">
                 <div>
-                    <h2 className="text-white text-2xl font-bold">Savings</h2>
+                    <div className="flex justify-between items-center">
+                        <h2 className="text-white text-2xl font-bold">Savings</h2>
+                        <button className='bg-white py-3 px-6 md:block hidden rounded-lg text-black font-medium'>Create Savings</button>
+                    </div>
+
                     <div className="py-4 grid grid-cols-3 ">
                         <DashboardCard name="Total Savings" description="10.1% since last year" number={2000} />
                     </div>
@@ -18,13 +22,48 @@ const Savings = () => {
                         <Card className="bg-black border-none">
                             <CardHeader className="text-white">
                                 <CardTitle className="text-white">New House</CardTitle>
-                                <CardDescription className="text-[#CDCDCD]">10 months left</CardDescription>
+                                <CardDescription className="text-[#C1C1C1] text-sm">10 months left</CardDescription>
                             </CardHeader>
                             <CardContent>
-                                <Progress value={33} />
+                                <Progress value={33} className="bg-[#FC4B37]" />
                                 <div className="flex justify-between items-center py-4">
-                                    <h2>$1200</h2>
-                                    <h2>$12000</h2>
+                                    <h2 className="text-white">$1200</h2>
+                                    <h2 className="text-white">$12000</h2>
+                                </div>
+                                <div className="flex justify-end">
+                                    <span className="text-lg font-bold text-[#FC4B37]">TimeLock</span>
+                                </div>
+                            </CardContent>
+                        </Card>
+                        <Card className="bg-black border-none">
+                            <CardHeader className="text-white">
+                                <CardTitle className="text-white">New House</CardTitle>
+                                <CardDescription className="text-[#C1C1C1] text-sm">10 months left</CardDescription>
+                            </CardHeader>
+                            <CardContent>
+                                <Progress value={33} className="bg-[#FC4B37]" />
+                                <div className="flex justify-between items-center py-4">
+                                    <h2 className="text-white">$1200</h2>
+                                    <h2 className="text-white">$12000</h2>
+                                </div>
+                                <div className="flex justify-end">
+                                    <span className="text-lg font-bold text-[#FC4B37]">TimeLock</span>
+                                </div>
+                            </CardContent>
+                        </Card>
+                        <Card className="bg-black border-none">
+                            <CardHeader className="text-white">
+                                <CardTitle className="text-white">New House</CardTitle>
+                                <CardDescription className="text-[#C1C1C1] text-sm">10 months left</CardDescription>
+                            </CardHeader>
+                            <CardContent>
+                                <Progress value={33} className="bg-[#FC4B37]" />
+                                <div className="flex justify-between items-center py-4">
+                                    <h2 className="text-white">$1200</h2>
+                                    <h2 className="text-white">$12000</h2>
+                                </div>
+                                <div className="flex justify-end">
+                                    <span className="text-lg font-bold text-[#FC4B37]">TimeLock</span>
                                 </div>
                             </CardContent>
                         </Card>
