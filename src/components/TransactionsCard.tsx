6cf1a6984e87a5581dfa4f98fcea9998f08a2faf
OFUZOR CHUKWUEMEKE <uniben2018@gmail.com>
2025-01-15 18:40:27 +0000

    refactor

--- src/components/TransactionsCard.tsx
+++ src/components/TransactionsCard.tsx
@@ -1,6 +1,14 @@
 import React from "react"
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '@/components/ui/card'
-const TransactionCard = ({ name,description }: { name: string,description:string }) => {
+const TransactionCard = ({ name, description }: { name: string, description: string }) => {
+    const data = [
+        { name: "Daily Savings", address: "0x39820x0awedc343dd", amount: 1200, coin: "USDT(SOL)" },
+        { name: "Daily Savings", address: "0x39820x0awedc343dd", amount: 2000, coin: "ETH(BASE)" },
+        { name: "Daily Savings", address: "0x39820x0awedc343dd", amount: 1200, coin: "USDC(SUI)" },
+        { name: "Daily Savings", address: "0x39820x0awedc343dd", amount: 120, coin: "USDT(SOL)" },
+        { name: "Daily Savings", address: "0x39820x0awedc343dd", amount: 1200, coin: "USDC(SUI)" },
+        { name: "Daily Savings", address: "0x39820x0awedc343dd", amount: 120, coin: "USDT(SOL)" },
+    ]
     return (
         <Card className='bg-black border-none'>
             <CardHeader className='text-white'>
@@ -9,47 +17,18 @@ const TransactionCard = ({ name,description }: { name: string,description:string
             </CardHeader>
             <CardContent>
                 <div className='space-y-6'>
-                    <div className='flex justify-between items-center cursor-pointer'>
-                        <div>
-                            <h2 className='text-white text-xl font-bold'>Daily Savings</h2>
-                            <p className='text-white text-sm'>0x39820x0awedc343dd</p>
+                    {data.map(({ name, address, amount, coin }) => (
+                        <div className='flex justify-between items-center cursor-pointer'>
+                            <div>
+                                <h2 className='text-white text-xl font-bold'>{name}</h2>
+                                <p className='text-white text-sm'>{address}</p>
+                            </div>
+                            <div>
+                                <h2 className='text-[#C6C2C2] text-[18px] font-bold'>-${amount}</h2>
+                                <p className='font-light text-sm text-right text-[#C6C2C2]'>{coin}</p>
+                            </div>
                         </div>
-                        <div>
-                            <h2 className='text-[#C6C2C2] text-[18px] font-bold'>-$1,200</h2>
-                            <p className='font-light text-sm text-right text-[#C6C2C2]'>USDT(sol)</p>
-                        </div>
-
-                    </div>
-                    <div className='flex justify-between items-center cursor-pointer'>
-                        <div>
-                            <h2 className='text-white text-xl font-bold'>Daily Savings</h2>
-                            <p className='text-white text-sm'>0x39820x0awedc343dd</p>
-                        </div>
-                        <div>
-                            <h2 className='text-[#C6C2C2] text-[18px] font-bold'>-$1,200</h2>
-                            <p className='font-light text-sm text-right text-[#C6C2C2]'>ETH(base)</p>
-                        </div>
-                    </div>
-                    <div className='flex justify-between items-center cursor-pointer'>
-                        <div>
-                            <h2 className='text-white text-xl font-bold'>House savings</h2>
-                            <p className='text-white text-sm'>0x39820x0awedc343dd</p>
-                        </div>
-                        <div>
-                            <h2 className='text-[#C6C2C2] text-[18px] font-bold'>-$1,200</h2>
-                            <p className='font-light text-sm text-right text-[#C6C2C2]'>USDT(sui)</p>
-                        </div>
-                    </div>
-                    <div className='flex justify-between items-center cursor-pointer'>
-                        <div>
-                            <h2 className='text-white text-xl font-bold'>Monthly Savings</h2>
-                            <p className='text-white text-sm'>0x39820x0awedc343dd</p>
-                        </div>
-                        <div>
-                            <h2 className='text-[#C6C2C2] text-[18px] font-bold'>-$1,200</h2>
-                            <p className='font-light text-sm text-right text-[#C6C2C2]'>USDT(sui)</p>
-                        </div>
-                    </div>
+                    ))}
                 </div>
             </CardContent>
         </Card>
